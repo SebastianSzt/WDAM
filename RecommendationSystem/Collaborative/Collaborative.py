3830259e23012f5ea6b3ce7cf00c6dc226c0a866
SebastianSzt <313061@stud.umk.pl>
2024-06-15 19:36:48 +0200

    RMSE + bigger data set

--- RecommendationSystem/Collaborative/Collaborative.py
+++ RecommendationSystem/Collaborative/Collaborative.py
@@ -3,9 +3,12 @@ from surprise import Dataset, Reader, SVD
 from surprise.model_selection import train_test_split
 from surprise import accuracy
 
-# Wczytanie danych z plików CSV
-movies = pd.read_csv("RecommendationSystem/Collaborative/movies.csv", usecols=["movieId", "title"])
-ratings = pd.read_csv("RecommendationSystem/Collaborative/ratings.csv", usecols=["userId", "movieId", "rating"])
+# Wczytanie danych z plików CSV ze strony: https://www.kaggle.com/datasets/grouplens/movielens-20m-dataset/data
+# movies = pd.read_csv("RecommendationSystem/Collaborative/movies.csv", usecols=["movieId", "title"])
+# ratings = pd.read_csv("RecommendationSystem/Collaborative/ratings.csv", usecols=["userId", "movieId", "rating"])
+
+movies = pd.read_csv("RecommendationSystem/Collaborative/movies-small.csv", usecols=["movieId", "title"])
+ratings = pd.read_csv("RecommendationSystem/Collaborative/ratings-small.csv", usecols=["userId", "movieId", "rating"])
 
 # Zbieranie danych od użytkownika
 print("Filmy są dostępne w bazie danych. Wybierz co najmniej 5 filmów, które oglądałeś.")
@@ -47,23 +50,8 @@ user_ratings_test = {
 
 num_recommendations = int(input("Ile rekomendacji chcesz otrzymać? (podaj liczbę): "))
 
-# Przygotowanie danych do biblioteki surprise
-reader = Reader(rating_scale=(0.5, 5.0))
-data = Dataset.load_from_df(ratings, reader)
-
-# Podział danych na zestaw treningowy i testowy
-trainset, testset = train_test_split(data, test_size=0.2)
-
-# Trenowanie modelu SVD
-model = SVD()
-model.fit(trainset)
-
-# Ewaluacja modelu
-predictions = model.test(testset)
-accuracy.rmse(predictions)
-
 # Funkcja rekomendująca filmy
-def recommend_movies(user_ratings, n_recommendations=5):
+def recommend_movies(user_ratings, num_recommendations, calculate_rmse=False):
     # Przypisanie nowego ID użytkownika
     user_id = ratings['userId'].max() + 1
     new_ratings = ratings.copy()
@@ -77,12 +65,25 @@ def recommend_movies(user_ratings, n_recommendations=5):
         # Dodanie nowego wiersza do zbioru ocen
         new_ratings = pd.concat([new_ratings, new_row], ignore_index=True)
     
-    # Ponowne przygotowanie danych dla biblioteki surprise
+    # Przygotowanie danych do biblioteki surprise
+    reader = Reader(rating_scale=(0.5, 5.0))
     data = Dataset.load_from_df(new_ratings, reader)
-    # Budowanie pełnego zbioru treningowego z nowych danych
-    trainset = data.build_full_trainset()
-    # Trenowanie modelu SVD na nowym zbiorze danych
-    model.fit(trainset)
+   
+    if calculate_rmse:
+        # Podział danych na zestaw treningowy i testowy
+        trainset, testset = train_test_split(data, test_size=0.2)
+        # Trenowanie modelu SVD na nowym zbiorze danych
+        svd = SVD()
+        svd.fit(trainset)
+        # Kalkulacja RMSE
+        predictions = svd.test(testset)
+        rmse = accuracy.rmse(predictions)
+    else:
+        # Budowanie pełnego zbioru treningowego z nowych danych
+        trainset = data.build_full_trainset()
+        # Trenowanie modelu SVD na nowym zbiorze danych
+        svd = SVD()
+        svd.fit(trainset)
     
     # Pobranie unikalnych ID filmów
     movie_ids = movies['movieId'].unique()
@@ -94,13 +95,13 @@ def recommend_movies(user_ratings, n_recommendations=5):
     for movie_id in movie_ids:
         if movie_id not in watched_movie_ids:
             # Przewidywanie oceny filmu dla nowego użytkownika
-            est = model.predict(user_id, movie_id).est
+            est = svd.predict(user_id, movie_id).est
             recommendations.append((movie_id, est))
     
     # Sortowanie rekomendacji według przewidywanej oceny w porządku malejącym
     recommendations.sort(key=lambda x: x[1], reverse=True)
     # Wybieranie ID najlepszych rekomendacji
-    recommended_movie_ids = [rec[0] for rec in recommendations[:n_recommendations]]
+    recommended_movie_ids = [rec[0] for rec in recommendations[:num_recommendations]]
     # Pobranie tytułów rekomendowanych filmów na podstawie ich ID
     recommended_movies = movies[movies['movieId'].isin(recommended_movie_ids)]
     
